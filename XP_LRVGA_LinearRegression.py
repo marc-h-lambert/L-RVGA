64545c90e40323102dae53b0684e71badc16592e
marc-h-lambert <62938851+marc-h-lambert@users.noreply.github.com>
2022-11-02 08:37:27 +0100

    add the init trick

--- XP_LRVGA_LinearRegression.py
+++ XP_LRVGA_LinearRegression.py
@@ -8,6 +8,7 @@
 ###################################################################################
 
 import numpy as np
+import numpy.linalg as LA
 from KalmanMachine.KDataGenerator import LinearRegObservations
 from KalmanMachine.Kalman4LinearReg import LKFLinReg, LargeScaleLKFLinReg
 from KalmanMachine.KEvalPosterior import PosteriorLinReg
@@ -25,7 +26,7 @@ def XP_HighDim_LinReg_Lattent(axs,sigma0,mu0,sigmaNoise,N,d,list_p,c,ppca,svd,nb
     y,X=RegObs.datas
     
     ################### GROUND TRUTH ####################### 
-    theta0=mu0*np.ones([d,1])/math.sqrt(d) 
+    theta0=mu0*np.ones([d,1])
     Cov0=np.identity(d)*sigma0**2
     posterior=PosteriorLinReg(theta0,Cov0).fit(X,y.reshape(N,),sigmaNoise)
     
@@ -42,20 +43,22 @@ def XP_HighDim_LinReg_Lattent(axs,sigma0,mu0,sigmaNoise,N,d,list_p,c,ppca,svd,nb
     print("Time per iteration for LKF is {0} s;".format(lkf.timePerIteration))
     print("The KL divergence for LKF is {0}".format(posterior.divergence(lkf.theta,lkf.Cov)))
     
-    ################### INITIALIZATION  & RUN LARGE SCALE KALMAN ####################### 
-    std0=1/sigma0   
-    psi0=std0**2*np.ones([d,1])
-
+    ################### INITIALIZATION  & RUN LARGE SCALE KALMAN #######################  
+    eps=1e-8
+    psi0=np.ones([d,1])*(1-eps)*1/(sigma0**2) 
     list_lskf=[]
     for p in list_p:
         np.random.seed(seed)
         if p==0 or svd:
-            B0=np.zeros([d,p])
+            W0=np.zeros([d,p])
         else:
-            B0=1e-2*np.random.multivariate_normal(np.zeros(p),np.identity(p),(d))
+            W0=np.random.multivariate_normal(np.zeros(p),np.identity(p),(d))
+            for i in range(0,p):
+                w0=math.sqrt(eps*d/p)/sigma0
+                W0[:,i]=W0[:,i]*w0/LA.norm(W0[:,i])
 
         tic=time.perf_counter()
-        lskf = LargeScaleLKFLinReg(theta0, psi0, B0, passNumber=1, sigma=1, ppca=ppca, svd=False, \
+        lskf = LargeScaleLKFLinReg(theta0, psi0, W0, passNumber=1, sigma=1, ppca=ppca, svd=False, \
                                    nbInnerLoop=nbInnerLoop).fit(X, y.reshape(N,),monitor=True)
         toc=time.perf_counter()
 
@@ -72,11 +75,46 @@ def XP_HighDim_LinReg_Lattent(axs,sigma0,mu0,sigmaNoise,N,d,list_p,c,ppca,svd,nb
     return lkf_perfo_time,lkf_perfo_memory
     
 if __name__=="__main__":
-    Test=["LinHD2"] # change the label to change the Section tested
+    Test=["LinHD"] # change the label to change the Section tested
     num=1    
     
+
+    
+    # LINEAR REGRESSION: test on p
+    # We have reduced the dimension to speed up, to find exactely paper result 
+    # put d=1000 and N=3000
+    if 'LinHD' in Test:
+         # The recusrive EM converge to the linear Kalman filter for p higher enough 
+        print("######### LinHD2 : Sensitivity to lattent dimension p ##############")
+        d=100 #1000
+        N=10000 #3000
+        nbInnerLoop=3 
+        
+        sigma0=1
+        mu0=0
+        sigmaNoise=1
+        list_p=[50,20,10,1]
+        c=1
+        seed=1
+        ppca=False
+        svd=False                 
+        
+        fig, ax = plt.subplots(1, 1, sharex=False,figsize=set_size(ratio=1,fraction=0.7),num=num)
+        lkf_perfo_time,lkf_perfo_memory=XP_HighDim_LinReg_Lattent(np.array([ax]),sigma0,mu0,sigmaNoise,N,d,list_p,c,ppca,svd,nbInnerLoop,seed,label=True)
+        ax.set_title("$N={0}$, $d={1}$, $InLoop={2}$".format(N,d,nbInnerLoop))
+        ax.set_yscale('log')
+        ax.xaxis.set_major_locator(MultipleLocator(1000))
+        ax.yaxis.set_major_locator(LogLocator(numticks=4))
+        ax.grid()
+        ax.legend(loc="lower left", ncol=3)
+        ax.set_xlabel('number of iterations')
+        ax.set_ylabel('KL')
+        plt.tight_layout()
+        plt.savefig('./outputs/LRVGA_LinReg_KL_{0}_{1}_{2}'.format(N,d,nbInnerLoop))
+        num=num+1
+        
     # LINEAR REGRESSION: test on inner loops
-    if 'LinHD1' in Test:
+    if 'LinHDInnerLoop' in Test:
         print("######### LinHD1 : Sensitivity to number of inner loops ##############")
         sigma0=1
         mu0=0
@@ -106,37 +144,4 @@ if __name__=="__main__":
             i=i+1
                 
         plt.savefig('./outputs/LSLinReg_innersLoops')   
-        num=num+1
-    
-    # LINEAR REGRESSION: test on p
-    # We have reduced the dimension to speed up, to find exactely paper result 
-    # put d=1000 and N=3000
-    if 'LinHD2' in Test:
-         # The recusrive EM converge to the linear Kalman filter for p higher enough 
-        print("######### LinHD2 : Sensitivity to lattent dimension p ##############")
-        d=100 #1000
-        N=1000 #3000
-        nbInnerLoop=3 
-        
-        sigma0=1
-        mu0=0
-        sigmaNoise=1
-        list_p=[100,10,2,1]
-        c=1
-        seed=1
-        ppca=False
-        svd=False                 
-        
-        fig, ax = plt.subplots(1, 1, sharex=False,figsize=set_size(ratio=1,fraction=0.7),num=num)
-        lkf_perfo_time,lkf_perfo_memory=XP_HighDim_LinReg_Lattent(np.array([ax]),sigma0,mu0,sigmaNoise,N,d,list_p,c,ppca,svd,nbInnerLoop,seed,label=True)
-        ax.set_title("$N={0}$, $d={1}$, $InLoop={2}$".format(N,d,nbInnerLoop))
-        ax.set_yscale('log')
-        ax.xaxis.set_major_locator(MultipleLocator(1000))
-        ax.yaxis.set_major_locator(LogLocator(numticks=4))
-        ax.grid()
-        ax.legend(loc="lower left", ncol=3)
-        ax.set_xlabel('number of iterations')
-        ax.set_ylabel('KL')
-        plt.tight_layout()
-        plt.savefig('./outputs/LRVGA_LinReg_KL_{0}_{1}_{2}'.format(N,d,nbInnerLoop))
         num=num+1
